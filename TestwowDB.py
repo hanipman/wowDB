8133d7649d1fddecc636e8c2da0c5681799cdac7
Chris Boado <boadoct@vt.edu>
2020-09-16 07:35:00 -0400

    refined classes and tests all possibilities with exceptions

--- TestwowDB.py
+++ TestwowDB.py
@@ -1,39 +1,53 @@
 import pytest
 from wowDB import WowDB
+from wowapi.exceptions import *
+
+locale = 'en_US'
+region = 'us'
+realm = 'Area 52'
+client_id = ''
+client_secret = ''
 
 class TestwowDB():
 
     def test_init(self):
-        wow = WowDB()
-        assert wow.getLocale() == 'en_US'
-        assert wow.getRegion() == 'us'
-        assert wow.getRealm() == 'Area 52'
+        '''Test init constructor with existing inputs'''
+        wow = WowDB(locale, region, realm, client_id, client_secret)
+        assert wow.getLocale() == locale
+        assert wow.getRegion() == region
+        assert wow.getRealm() == realm
+        assert wow.getRealmSlug() == 'area-52'
+        assert wow.getConnectedRealmID() == 3676
 
-    def test_getRealmID(self):
-        wow = WowDB()
-        data = wow.getRealmID()
-        assert data == 1566
+    @pytest.mark.parametrize(
+        "locale,region,realm,client_id,client_secret,expected", 
+        [
+            (locale,region,realm,'',client_secret,pytest.raises(WowApiOauthException)),
+            (locale,region,realm,client_id,'',pytest.raises(WowApiOauthException)),
+            ('',region,realm,client_id,client_secret,pytest.raises(ValueError)),
+            (locale,'',realm,client_id,client_secret,pytest.raises(ValueError)),
+            (locale,region,'',client_id,client_secret,pytest.raises(ValueError))
+        ])
+    def test_init_missing_server_info_client_cred(self, locale, region, realm, client_id, client_secret, expected):
+        '''Test init constructor for missing locale, region or realm.'''
+        with expected:
+            assert WowDB(locale, region, realm, client_id, client_secret) is not None
 
-    def test_getRealmSlug(self):
-        wow = WowDB()
-        data = wow.getRealmSlug()
-        assert data == 'area-52'
-
-    def test_getConnectedRealm(self):
-        wow = WowDB()
-        wow.getRealmSlug()
-        data = wow.getConnectedRealm()
-        assert data == 3676
+    @pytest.mark.parametrize(
+        "locale,region,realm,client_id,client_secret,expected", 
+        [
+            (locale,region,realm,'blah',client_secret,pytest.raises(WowApiOauthException)),
+            (locale,region,realm,client_id,'blah',pytest.raises(WowApiOauthException)),
+            ('blah',region,realm,client_id,client_secret,pytest.raises(WowApiException)),
+            (locale,'blah',realm,client_id,client_secret,pytest.raises(WowApiException)),
+            (locale,region,'blah',client_id,client_secret,pytest.raises(WowApiException))
+        ])
+    def test_init_bad_server_info_client_cred(self, locale, region, realm, client_id, client_secret, expected):
+        '''Test init constructor for missing locale, region or realm.'''
+        with expected:
+            assert WowDB(locale, region, realm, client_id, client_secret) is not None
 
     def test_getAuctions(self):
-        wow = WowDB()
-        wow.getRealmID()
-        wow.getRealmSlug()
-        wow.getConnectedRealm()
-        data = wow.getAuctions()
-        assert 'auctions' in data
-        
-
-    # def test_getAuctions(self):
-    #     wow = WowDB()
-    #     data = wow.getAuction
+        wow = WowDB(locale, region, realm, client_id, client_secret)
+        data = wow.findAuctions()
+        assert 'auctions' in data.keys()
--- wowDB.py
+++ wowDB.py
@@ -1,79 +1,101 @@
 
 from pprint import pprint
 from wowapi import WowApi
+from wowapi.exceptions import *
 import re
-import time
 
 class WowDB:
-    def __init__(self):
+    def __init__(self, locale, region, realm, client_id, client_secret):
+        if client_id or client_secret:
+            try:
+                self.api = WowApi(client_id, client_secret)
+            except WowApiOauthException as e:
+                print(e.args)
+                raise e
+            except Exception as e:
+                print(e.args)
+                raise e
+        else:
+            raise ValueError('client_id or client_secret empty.')
+        if locale:
+            self.locale = locale
+        else:
+            raise ValueError('locale invalid')
+        if region:
+            self.region = region
+        else:
+            raise ValueError('region invalid')
+        if realm:
+            self.realm = realm
+        else:
+            raise ValueError('locale, region, and/or realm empty.')
         try:
-            file = open('bliz_cred.txt', 'r')
-            lines = file.readlines()
-            if not lines:
-                raise ValueError('No arguments found. File \'bliz_cred.txt\' empty.')
-            file.close()
-
-            for line in lines:
-                tokens = line.split('=')
-                tokens = [x.strip() for x in tokens]
-                if (tokens[0] == "locale"):
-                    self.locale = tokens[1]
-                elif (tokens[0] == "region"):
-                    self.region = tokens[1]
-                elif (tokens[0] == "realm"):
-                    self.realm = tokens[1]
-                elif (tokens[0] == "client_id"):
-                    self.client_id = tokens[1]
-                elif (tokens[0] == "client_secret"):
-                    self.client_secret = tokens[1]
-            
-            if (self.locale == "" or
-                self.region == "" or
-                self.realm == "" or
-                self.client_id == "" or
-                self.client_secret == ""):
-                raise ValueError("Missing information in file \'bliz_cred.txt\'.")
-
-            self.api = WowApi(self.client_id, self.client_secret)
-
-        except FileNotFoundError as e:
+            self.__findConnectedRealm()
+        except WowApiOauthException as e:
             print(e.args)
-        except ValueError as e:
-            print(e.args)
+            raise e
         except Exception as e:
             print(e.args)
-            
-    def getRealmID(self):
-        data = self.api.get_realm_index(region=self.region, namespace='dynamic-us', locale=self.locale)
-        for x in data['realms']:
-            if x['name'] == self.realm:
-                self.realm_id = x['id']
-                return x['id']
-        return -1
+            raise e
 
-    def getRealmSlug(self):
-        data = self.api.get_realm_index(region=self.region, namespace='dynamic-us', locale=self.locale)
-        for x in data['realms']:
-            if x['name'] == self.realm:
-                self.realm_slug = x['slug']
-                return x['slug']
-        return -1
-
-    def getConnectedRealm(self):
-        data = None
+    def __findRealmID(self):
+        self.realm_id = None
         try:
+            data = self.api.get_realm_index(region=self.region, namespace='dynamic-us', locale=self.locale)
+            for x in data['realms']:
+                if x['name'] == self.realm:
+                    self.realm_id = x['id']
+        except WowApiException as e:
+            print(e.arts)
+            raise e
+        except Exception as e:
+            print(e.args)
+            raise e
+        
+    def __findRealmSlug(self):
+        self.realm_slug = None
+        try:
+            data = self.api.get_realm_index(region=self.region, namespace='dynamic-us', locale=self.locale)
+            for x in data['realms']:
+                if x['name'] == self.realm:
+                    self.realm_slug = x['slug']
+        except WowApiException as e:
+            print(e.args)
+            raise e
+        except Exception as e:
+            print(e.args)
+            raise e
+
+    def __findConnectedRealm(self, realm_slug=None):
+        self.connected_realm_id = None
+        try:
+            if realm_slug == None:
+                self.__findRealmSlug()
             data = self.api.get_realm(region=self.region, namespace='dynamic-us', locale=self.locale, realm_slug=self.realm_slug)
             data = data['connected_realm']['href']
             data = re.search(r'\d+', data).group()
             self.connected_realm_id = int(data)
+        except WowApiException as e:
+            print(e.args)
+            raise e
         except Exception as e:
             print(e.args)
-        return int(data)
+            raise e
 
-    def getAuctions(self):
-        data = self.api.get_auctions(region=self.region, namespace='dynamic-us', locale=self.locale, connected_realm_id=self.connected_realm_id)
-        return data
-
+    def findAuctions(self, connected_realm_id=None):
+        data = None
+        try:
+            if connected_realm_id == None:
+                self.__findConnectedRealm()
+            data = self.api.get_auctions(region=self.region, namespace='dynamic-us', locale=self.locale, connected_realm_id=self.connected_realm_id)
+            return data
+        except WowApiException as e:
+            print(e.args)
+            raise e
+        except Exception as e:
+            print(e.args)
+            raise e
+    
     def getLocale(self):
         return self.locale
     
@@ -83,11 +105,14 @@ class WowDB:
     def getRealm(self):
         return self.realm
 
-    def getClientID(self):
-        return self.client_id
+    def getRealmID(self):
+        return self.realm_id
 
-    def getClientSecret(self):
-        return self.client_secret
+    def getRealmSlug(self):
+        return self.realm_slug
+
+    def getConnectedRealmID(self):
+        return self.connected_realm_id
 
 def main():
     wow = WowDB()
