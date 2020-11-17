dc3a6bb31efff7f71e35470c2e07d7874a251850
Chris Boado <boadoct@vt.edu>
2020-11-17 18:46:43 -0500

    change from single connection to connection pool

--- dbConnect.py
+++ dbConnect.py
@@ -1,5 +1,5 @@
 import psycopg2
-from psycopg2 import sql
+from psycopg2 import pool,sql
 from configparser import ConfigParser
 import logging
 
@@ -38,12 +38,12 @@ class dbConnect():
     Class to connect with DB and execute queries specific to wowDB
     '''
     def __init__(self):
-        self.conn = None
+        self.conn_pool = None
 
     def __del__(self):
-        self.conn.close()
+        self.conn_pool.closeall()
 
-    def connect(self, host, database, user, password):
+    def connect(self, host, database, port, user, password):
         '''
         Connect to database using details within settings.ini
         
@@ -51,7 +51,8 @@ class dbConnect():
         @throws Exception Thrown when any other exception is caught
         '''
         try:
-            self.conn = psycopg2.connect(host=host, dbname=database, user=user, password=password)
+            self.conn_pool = psycopg2.pool.ThreadedConnectionPool(5,100,host=host,database=database,user=user,password=password,port=port)
+            logging.debug("Connected to database {0} at {1} port {2}".format(database, host, port))
         except (Exception, psycopg2.Error) as e:
             logging.exception(str(e))
             raise e
@@ -69,27 +70,31 @@ class dbConnect():
         '''
         try:
             self.realm = realm_slug.replace('-','_')
-            cur = self.conn.cursor()
-            cur.execute(sql.SQL(
-                """
-                CREATE TABLE IF NOT EXISTS {} (
-                    interval TIMESTAMP NOT NULL,
-                    item_id INTEGER NOT NULL,
-                    quantity INTEGER NOT NULL,
-                    avg_unit_price BIGINT NOT NULL,
-                    std_dev BIGINT NOT NULL,
-                    high_price BIGINT NOT NULL,
-                    low_price BIGINT NOT NULL
-                );
-                CREATE TABLE IF NOT EXISTS item_list (
-                    item_id INTEGER NOT NULL,
-                    item_name TEXT NOT NULL,
-                    item_pic BYTEA NOT NULL
+            local_conn = self.conn_pool.getconn()
+            if local_conn:
+                cur = local_conn.cursor()
+                cur.execute(sql.SQL(
+                    """
+                    CREATE TABLE IF NOT EXISTS {} (
+                        interval TIMESTAMP NOT NULL,
+                        item_id INTEGER NOT NULL,
+                        quantity INTEGER NOT NULL,
+                        avg_unit_price BIGINT NOT NULL,
+                        std_dev BIGINT NOT NULL,
+                        high_price BIGINT NOT NULL,
+                        low_price BIGINT NOT NULL
+                    );
+                    CREATE TABLE IF NOT EXISTS item_list (
+                        item_id INTEGER NOT NULL,
+                        item_name TEXT NOT NULL,
+                        item_pic BYTEA NOT NULL
+                    )
+                    """).format(sql.Identifier(self.realm)),[]
                 )
-                """).format(sql.Identifier(self.realm)),[]
-            )
-            self.conn.commit()
-            cur.close()
+                local_conn.commit()
+                cur.close()
+                self.conn_pool.putconn(local_conn)
+                logging.debug("Created table %s and table item_list" % self.realm)
         except (Exception, psycopg2.Error) as e:
             logging.exception(str(e))
             raise e
@@ -104,35 +109,38 @@ class dbConnect():
         @throws Exception Thrown when any other exception is caught
         '''
         try:
-            cur = self.conn.cursor()
-            cur.execute(sql.SQL(
-                """
-                INSERT INTO {} (interval,
-                                item_id,
-                                quantity,
-                                avg_unit_price,
-                                std_dev,
-                                high_price,
-                                low_price)
-                    VALUES (current_timestamp,
-                            %s,
-                            %s,
-                            %s,
-                            %s,
-                            %s,
-                            %s
-                    )
-                """).format(sql.Identifier(self.realm)), [
-                    item['item_id'],
-                    item['quantity'],
-                    item['avg_unit_price'],
-                    item['std_dev'],
-                    item['high_price'],
-                    item['low_price']
-                ]
-            )
-            self.conn.commit()
-            cur.close()
+            local_conn = self.conn_pool.getconn()
+            if local_conn:
+                cur = local_conn.cursor()
+                cur.execute(sql.SQL(
+                    """
+                    INSERT INTO {} (interval,
+                                    item_id,
+                                    quantity,
+                                    avg_unit_price,
+                                    std_dev,
+                                    high_price,
+                                    low_price)
+                        VALUES (current_timestamp,
+                                %s,
+                                %s,
+                                %s,
+                                %s,
+                                %s,
+                                %s
+                        )
+                    """).format(sql.Identifier(self.realm)), [
+                        item['item_id'],
+                        item['quantity'],
+                        item['avg_unit_price'],
+                        item['std_dev'],
+                        item['high_price'],
+                        item['low_price']
+                    ]
+                )
+                local_conn.commit()
+                cur.close()
+                self.conn_pool.putconn(local_conn)
         except (Exception, psycopg2.Error) as e:
             logging.exception(str(e))
             raise e
@@ -149,20 +157,23 @@ class dbConnect():
         @throws Exception Thrown when any other exception is caught
         '''
         try:
-            cur = self.conn.cursor()
-            cur.execute(
-                """
-                SELECT EXISTS (
-                    SELECT *
-                    FROM item_list
-                    WHERE item_id = %s
+            local_conn = self.conn_pool.getconn()
+            if local_conn:
+                cur = local_conn.cursor()
+                cur.execute(
+                    """
+                    SELECT EXISTS (
+                        SELECT *
+                        FROM item_list
+                        WHERE item_id = %s
+                    )
+                    """,
+                    (item_id,)
                 )
-                """,
-                (item_id,)
-            )
-            res = cur.fetchone()[0]
-            cur.close()
-            return res
+                res = cur.fetchone()[0]
+                cur.close()
+                self.conn_pool.putconn(local_conn)
+                return res
         except (Exception, psycopg2.Error) as e:
             logging.exception(str(e))
             raise e
@@ -179,23 +190,27 @@ class dbConnect():
         @throws Exception Thrown when any other exception is caught
         '''
         try:
-            cur = self.conn.cursor()
-            cur.execute(
-                """
-                INSERT INTO item_list
-                    (item_id, item_name, item_pic)
-                SELECT %s, %s, %s
-                WHERE
-                    NOT EXISTS (
-                        SELECT item_id
-                        FROM item_list
-                        WHERE item_id = %s
-                    )
-                """,
-                (item_id, item_name, item_pic, item_id)
-            )
-            self.conn.commit()
-            cur.close()
+            local_conn = self.conn_pool.getconn()
+            if local_conn:
+                cur = local_conn.cursor()
+                cur.execute(
+                    """
+                    INSERT INTO item_list
+                        (item_id, item_name, item_pic)
+                    SELECT %s, %s, %s
+                    WHERE
+                        NOT EXISTS (
+                            SELECT item_id
+                            FROM item_list
+                            WHERE item_id = %s
+                        )
+                    """,
+                    (item_id, item_name, item_pic, item_id)
+                )
+                local_conn.commit()
+                cur.close()
+                self.conn_pool.putconn(local_conn)
+                logging.debug("Storing item %s name and picture in table item_list" % item_id)
         except (Exception, psycopg2.Error) as e:
             logging.exception(str(e))
             raise e
